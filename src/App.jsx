dcb3f48eabfd1bbf7f0446aa9f7796b1ccbaf320
TejashPithadiya <tejashpithadiya5951@gmail.com>
2024-12-29 15:09:31 +0530

    tejash file add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tejash from './Tejash'
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <p>This is my app</p>
       <div>
       i am Jaydip bela
+      <Tejash/>
     </div>
     </div>
   )
---
+++ src/Tejash.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Tejash = () => {
+  return (
+    <div>
+     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae natus commodi excepturi sint a totam quas, esse tempora exercitationem veritatis doloremque odit, laborum, quam fuga ad doloribus? Eum, ipsa corrupti!</p>
+     <button>click me</button>
+    </div>
+  )
+}
+
+export default Tejash
