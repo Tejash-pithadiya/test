7ae962c86170ee2c40ea6f0adb3bbeda2d068a31
TejashPithadiya <tejashpithadiya5951@gmail.com>
2024-12-30 12:13:14 +0530

    dfdvd

--- src/Tejash.jsx
+++ src/Tejash.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Tejash = () => {
   return (
     <div>
-     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae natus commodi excepturi sint a totam quas, esse tempora exercitationem veritatis doloremque odit, laborum, quam fuga ad doloribus? Eum, ipsa corrupti!</p>
+     {/* <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae natus commodi excepturi sint a totam quas, esse tempora exercitationem veritatis doloremque odit, laborum, quam fuga ad doloribus? Eum, ipsa corrupti!</p> */}
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis fuga at quasi hic fugit voluptate? Temporibus distinctio corporis placeat maxime tempora dolorum molestias sed, cupiditate consequuntur suscipit autem ullam dolor?</p>
      <button>click me</button>
     </div>
   )
